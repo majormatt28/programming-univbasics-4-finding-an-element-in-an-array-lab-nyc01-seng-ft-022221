96a27bf6f487606596be00cfb0ea64a3bbd8848a
Mathew E Newton <elijah2895@hotmail.com>
2021-02-06 15:09:20 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -4,6 +4,7 @@ def find_element_index(array, value_to_find)
   found_value_index = nil
   while count < array.length do
     puts array[count] == value_to_find
+    found_value_index = count
     count += 1
   end
   found_value_index
