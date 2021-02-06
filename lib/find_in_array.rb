7b79c2cf5721c471d1baf72eb63ea321d5459185
Mathew E Newton <elijah2895@hotmail.com>
2021-02-06 15:04:56 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,4 +1,5 @@
 def find_element_index(array, value_to_find)
   # Add your solution here
-  find_element_index.include?(index)
+  find_element_index.include?(array)
+  find_element_index.include?(value_to_find)
 end
